15c5eea10acc87f69486c17bc43ece81420ce392
Sean P. Myrick V19.0.3.1 <65933340+seanpm2001@users.noreply.github.com>
2020-07-22 18:50:30 -0700

    MenuLayoutV1.py
    An archive of the first version of the menu layout script.

---
+++ OpenTowers/ProgramData/PixelTower/Tower1/Menu/OldVersions/MenuLayoutV1.py
@@ -0,0 +1,41 @@
+# Start of script
+"""
+Menu layout
+This script defines the menu layout within the game. It is styled to be like a mobile smartphone operating system. Unlike Tiny Tower, this version has more than 1 screen of options.
+"""
+print("Menu layout (CLI)")
+noMoreExit = input("You shouldn't be using this file directly. Please press [ENTER] to quit now")
+print("The window should be closed. If it still isn't, press the close button, or use a process manager and end this process")
+# Menu screen 1 of 2 (default) - I will make it in the future so you can change how many items appear on each menu screen
+option1 = str("Citizens") # A list of your towers citizens
+option2 = str("Settings") # A list of general settings
+option3 = str("More games") # Lists the other built-in game modes
+option4 = str("Achievements") # List of achievements, multiple sections are there for if you play the game across multiple platforms
+option5 = str("Friends") # A list of friends/people you add to visit daily
+option6 = str("Emerald Piggy bank") # Where bucks build up (1 per hour (1 for every floor per 1 hour)) Max Capacity: 1000000 bucks 
+option7 = str("Golden Piggy bank") # Where golden tickets build up (extremely slow, 1 for every 50 floors per 336 hours (2 weeks)) # Max capacity: 1000000 golden tickets
+option8 = str("Silver piggy bank") # Where coins build up (very fast, 1 per 10 seconds (1 for every floor per 10 seconds)) # Max capacity: 1000000000000000 coins
+option9 = str("Platinum piggy bank") # Where coins, bucks, and golden tickets are placed after the original 3 are full.
+option10 = str("Elevator") # Where you can control and upgrade your elevator settings
+option11 = str("Lobby") # Where you can manage and customize your lobby
+option12 = str("Roof") # Where you can customize your rooftop
+# Menu screen 2 of 2 (default) - I will make it in the future so you can change how many items appear on each menu screen
+option13 = str("Store") # In-app purchase and donation options for those who choose")
+option14 = str("Profile") # Your personal profile
+option15 = str("Cloud backup") # A list of cloud backup options and details
+option16 = str("Quests") # The old quests from old Tiny Tower, with the originals, along with many new ones")
+option17 = str("Stats") # The old stats menu from old Tiny Tower, with the original statistics, along with some new ones")
+option18 = str("PixBook") # A messaging service for your citizens and friends, can be sorted and filtered
+option19 = str("Raffle") # The golden tickets raffle, where you get a chance to earn golden tickets
+option20 = str("Advertisements") # A place where you can change advertisement settings with the SNU advertisement engine")
+option21 = str("Highlights") # A list of stuff that you can highlight. By default, daily tower pictures, game activity and messages are highlighted
+option22 = str("Update") # A built-in option that checks for (and can install) updates. Hot patching is also a planned feature
+option23 = str("Past towers") # A menu of your tower from the past, and also a basis of each change. You can go through and look at it as well.
+option24 = str("Rebuild") # A menu that gives you the option to rebuild your tower. To prevent accidental deletion, you have to confirm with a button, then confirm by typing "confirm" then pressing a button, and confirming with another button.
+"""
+File info
+File type: Python script
+Line count (including blank lines and compiler line): 42
+File version: 1 (Wednesday, July 22nd 2020 at 6:50 pm)
+"""
+# End of script
